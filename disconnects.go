a4941de4bf8e0bf0d51a87233b0565763e214504
Jürgen Legler <jogihoogi@gmail.com>
2020-12-31 00:43:54 +0100

    fix disconnects

--- disconnects.go
+++ disconnects.go
@@ -1,17 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func (parser *Parser) handlePlayerDisconnects() {
 	for _, p := range parser.mvd.State.Players {
+
 		if p.Spectator == true {
 			continue
 		}
 		if len(p.Name) == 0 {
 			continue
 		}
-		// TODO: not sure about this
-		if p.Entertime == 0 {
+
+		// TODO: not so sure
+		if p.Userid == 0 {
 			continue
 		}
+
 		parser.players[p.Userid] = p
 	}
 }
